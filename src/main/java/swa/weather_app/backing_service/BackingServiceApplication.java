7413305576463ae9891a62a22e9a34343ce7a412
ostapvad <ostapvad@fel.cvut.cz>
2023-07-22 02:57:19 +0200

    init

---
+++ src/main/java/swa/weather_app/backing_service/BackingServiceApplication.java
@@ -0,0 +1,13 @@
+package swa.weather_app.backing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackingServiceApplication {
+	//
+	public static void main(String[] args) {
+		SpringApplication.run(BackingServiceApplication.class, args);
+	}
+
+}
