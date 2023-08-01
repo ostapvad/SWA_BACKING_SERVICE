eea4d7bdd005ad245dc3aca53e33f543a481b0b1
ostapvad <ostapvad@fel.cvut.cz>
2023-08-01 02:56:06 +0200

    Docs are added

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 ## SWA Backing Service Implementation
-
+### How to run
+Create your docker image
 ```bash
-docker compose up
+mvn clean install -DskipTests=true
+docker build -t=YOUR_CONT_NAME .
+```
+Run docker postgres container 
+```
+docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=main -e POSTGRES_DB=measurements --name=postgres_cont postgres
+```
+Run backing service container
+```
+docker run  YOUR_CONT_NAME 
+```
+
+
+## ToDo list
+
+- [x] Basis functionality
+- [x] Docker integration
+- [ ] Resolve volume store for postgres 
+- [ ] 
+
--- src/main/java/swa/weather_app/backing_service/controller/MeasurementController.java
+++ src/main/java/swa/weather_app/backing_service/controller/MeasurementController.java
@@ -1,6 +1,13 @@
 package swa.weather_app.backing_service.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +17,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriUtils;
 import swa.weather_app.backing_service.dtos.WeatherMeasurementDTO;
+import swa.weather_app.backing_service.entity.ErrorMessage;
 import swa.weather_app.backing_service.entity.WeatherMeasurement;
 import swa.weather_app.backing_service.entity.WindData;
 import swa.weather_app.backing_service.error.CityMeasurementsAreNotFound;
 import swa.weather_app.backing_service.service.MeasurementService;
-
+import org.springframework.http.MediaType;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
+@Tag(name = "Backing service", description = "Backing service management APIs")
 public class MeasurementController {
 
 
@@ -32,32 +41,42 @@ public class MeasurementController {
     }
 
     @GetMapping("/")
+    @Operation(
+            summary = "Health check status")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Backing service is running", content =
+                    @Content(mediaType = MediaType.TEXT_PLAIN_VALUE, schema = @Schema(type = "string",
+                            format = "plain", example = "Backing service is OK."))),
+            @ApiResponse(responseCode = "500", description = "Internal error occurred", content = @Content())
+    })
     public ResponseEntity<String> healthCheck() {
         return new ResponseEntity<>("Backing service is OK.", HttpStatus.OK);
     }
 
-    @PostMapping("/measurements")
-    public ResponseEntity<WeatherMeasurement> saveMeasurement(@RequestBody WeatherMeasurementDTO measurementDTO) {
-        LOGGER.info(String.format("Posting new weather measurement of city %s", measurementDTO.getCity()));
-        var weatherMeasurement = WeatherMeasurement.builder()
-                .time(measurementDTO.getTime())
-                .city(measurementDTO.getCity())
-                .temperature((float) measurementDTO.getTemperature())
-                .pressure(measurementDTO.getPressure())
-                .humidity(measurementDTO.getHumidity()).wind(
-                WindData.builder().speed((float) measurementDTO.getWind().getSpeed())
-                        .gust((float) measurementDTO.getWind().getGust())
-                        .deg(measurementDTO.getWind().getDeg()).build()).build();
-        return ResponseEntity.status(HttpStatus.CREATED).body(measurementService.saveNewMeasurement(weatherMeasurement));
-    }
-
     @GetMapping("/measurements/all")
+    @Operation(summary = "Retrieve all measurements from DB")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of measurements received",
+                    content = @Content(mediaType = "application/json", array =
+                    @ArraySchema(schema = @Schema(implementation = WeatherMeasurement.class)))),
+            @ApiResponse(responseCode = "500", description = "Internal error occurred", content = @Content())
+    })
     public ResponseEntity<List<WeatherMeasurement>> retrieveAllMeasurements() {
-
         return ResponseEntity.status(HttpStatus.OK).body(measurementService.retrieveAllMeasurements());
     }
 
     @GetMapping("/measurements")
+    @Operation(summary = "Find measurement by city and in time interval from and to")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of measurements received",
+                    content =
+                    @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = WeatherMeasurement.class)))),
+            @ApiResponse(responseCode = "404", description =
+                    "Weather measurements of city in the current time interval are not found", content =
+            @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(responseCode = "500", description = "Internal error occurred", content = @Content())
+    })
     public ResponseEntity<List<WeatherMeasurement>> getMeasurementsByCityAndTime(@RequestParam(name = "city")
                                                                                  String city,
                                                                                  @RequestParam(name = "from")
@@ -71,6 +90,27 @@ public class MeasurementController {
 
         List<WeatherMeasurement> foundMeasurements = measurementService.findByCityAndFromToTime(city, from, to);
         return ResponseEntity.status(HttpStatus.OK).body(foundMeasurements);
+    }
+    @PostMapping("/measurements")
+    @Operation(summary = "Save new measurement to DB")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Weather measurement is created in DB",
+                    content = @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = WeatherMeasurement.class))),
+            @ApiResponse(responseCode = "500", description = "Internal error occurred", content = @Content())
+    })
+    public ResponseEntity<WeatherMeasurement> saveMeasurement(@RequestBody WeatherMeasurementDTO measurementDTO) {
+        LOGGER.info(String.format("Posting new weather measurement of city %s", measurementDTO.getCity()));
+        var weatherMeasurement = WeatherMeasurement.builder()
+                .time(measurementDTO.getTime())
+                .city(measurementDTO.getCity())
+                .temperature((float) measurementDTO.getTemperature())
+                .pressure(measurementDTO.getPressure())
+                .humidity(measurementDTO.getHumidity()).wind(
+                        WindData.builder().speed((float) measurementDTO.getWind().getSpeed())
+                                .gust((float) measurementDTO.getWind().getGust())
+                                .deg(measurementDTO.getWind().getDeg()).build()).build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(measurementService.saveNewMeasurement(weatherMeasurement));
     }
 
 }
